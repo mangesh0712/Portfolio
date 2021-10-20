c4f7d3e1a799e37282186226f90a3d4d472b3822
Mangesh Khandale <mangeshkhandale327@gmail.com>
2021-10-20 13:08:30 +0530

    responsive changes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { PATHS } from '../data'
+import { PATHS } from "../data";
 
 function Profile() {
   return (
-    <div className="flex flex-col mt-10 items-center justify-center text-gray-300">
+    <div className="flex flex-col mt-2 md:mt-5 items-center justify-center text-gray-300">
       <img
         src={PATHS.PP}
         alt=""
-        className="rounded-full filter grayscale h-40 w-40 object-contain"
+        className="rounded-full filter grayscale h-28 w-28 md:h-40 md:w-40 object-contain"
       />
-      <span className="uppercase font-sans p-1 mt-4 text-xl font-bold ">
+      <span className="uppercase font-sans p-1 mt-1 md:mt-4 text-md md:text-xl font-bold ">
         mangesh khandale
       </span>
-      <span className="mb-3 font-serif uppercase text-md font-light">
+      <span className="mb-1 md:mb-3 font-serif uppercase text-sm md:text-md font-light">
         full-stack web developer
       </span>
     </div>
